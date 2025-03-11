dfb830d936f3ab93d99d5969cdb2ff35c1c02aec
Harvey <harvey199826@gmail.com>
2025-03-11 21:01:49 +0800

    final1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 // Import the MongoDB database connection module. Adjust the path based on your file structure.
-const mongodb = require('./cse341_1/data/database.js');
+const mongodb = require('./cse341_1/data/database.js'); //final
 
 
 
